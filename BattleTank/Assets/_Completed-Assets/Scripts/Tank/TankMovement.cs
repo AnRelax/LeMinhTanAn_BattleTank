29409df937e671ff315f5a273979a5e94b4e20a8
leminhtanan <leminhtanan.01072016@gmail.com>
2023-06-05 00:27:59 +0700

    Them Tanks vao Map va lam UI cho game

--- BattleTank/Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
+++ BattleTank/Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
@@ -4,40 +4,32 @@ namespace Complete
 {
     public class TankMovement : MonoBehaviour
     {
-        public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
-        public float m_Speed = 12f;                 // How fast the tank moves forward and back.
-        public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
-        public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-        public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
-        public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-		public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+        public int m_PlayerNumber = 1;
+        public float m_Speed = 12f;
+        public float m_TurnSpeed = 180f;
+        public AudioSource m_MovementAudio;
+        public AudioClip m_EngineIdling;
+        public AudioClip m_EngineDriving;
+		public float m_PitchRange = 0.2f;
 
-        private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-        private string m_TurnAxisName;              // The name of the input axis for turning.
-        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-        private float m_MovementInputValue;         // The current value of the movement input.
-        private float m_TurnInputValue;             // The current value of the turn input.
-        private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
-        private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
+        private string m_MovementAxisName;
+        private string m_TurnAxisName;
+        private Rigidbody m_Rigidbody;
+        private float m_MovementInputValue; 
+        private float m_TurnInputValue;
+        private float m_OriginalPitch;
+        private ParticleSystem[] m_particleSystems;
 
-        private void Awake ()
-        {
+        private void Awake (){
             m_Rigidbody = GetComponent<Rigidbody> ();
         }
 
 
-        private void OnEnable ()
-        {
-            // When the tank is turned on, make sure it's not kinematic.
+        private void OnEnable (){
             m_Rigidbody.isKinematic = false;
-
-            // Also reset the input values.
             m_MovementInputValue = 0f;
             m_TurnInputValue = 0f;
 
-            // We grab all the Particle systems child of that Tank to be able to Stop/Play them on Deactivate/Activate
-            // It is needed because we move the Tank when spawning it, and if the Particle System is playing while we do that
-            // it "think" it move from (0,0,0) to the spawn point, creating a huge trail of smoke
             m_particleSystems = GetComponentsInChildren<ParticleSystem>();
             for (int i = 0; i < m_particleSystems.Length; ++i)
             {
@@ -48,10 +40,7 @@ namespace Complete
 
         private void OnDisable ()
         {
-            // When the tank is turned off, set it to kinematic so it stops moving.
             m_Rigidbody.isKinematic = true;
-
-            // Stop all particle system so it "reset" it's position to the actual one instead of thinking we moved when spawning
             for(int i = 0; i < m_particleSystems.Length; ++i)
             {
                 m_particleSystems[i].Stop();
